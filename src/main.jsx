12c41c206f1d02b4f96fdc37a96b8125a6bf6394
dhruv-tilva <tilavadhruv111@gmail.com>
2025-03-24 18:45:59 +0530

    Fix the error

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
 import { registerSW } from "virtual:pwa-register";
 
 registerSW({ immediate: true });
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
